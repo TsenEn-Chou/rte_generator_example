0913181e2d32db5ec092ba0d9f1c19fda7f73a1d
TsenHill <chou870808@gmail.com>
2024-02-21 12:59:20 +0800

    first commit

---
+++ generated/rte_data_management.c
@@ -0,0 +1,17 @@
+#include "rte_data_management.h"
+
+MyUint8OfVendorID R1_Spd = 111;
+uint32 R1_Spd_first_reception_flag = 1U;
+
+MyUint8OfVendorID R2_Acc = 111;
+uint32 R2_Acc_first_reception_flag = 1U;
+
+#define R3_Temp_MAX_SIZE 10
+MyUint8OfVendorID R3_Temp[R3_Temp_MAX_SIZE];
+RingBuffer RB_R3_Temp = {
+    R3_Temp,
+    0,
+    0,
+    R3_Temp_MAX_SIZE,
+    0
+};
