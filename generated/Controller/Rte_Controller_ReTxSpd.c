9c03be58acf8ba7da11e9400115badf0421e34b6
TsenHill <chou870808@gmail.com>
2024-02-21 21:13:06 +0800

    5

--- generated/Controller/Rte_Controller_ReTxSpd.c
+++ generated/Controller/Rte_Controller_ReTxSpd.c
@@ -1,17 +1,22 @@
 #define RTE_RUNNABLEAPI_ReTxSpd
 #include "Rte_Controller.h" 
-#include "../rte_data_management.h"
-extern MyUint16OfVendorID R1_Spd;
+#include "../ioc_data_management.h"
 Std_ReturnType Rte_Write_PpIfVehSpd_Spd(Impl_uint16 data){
 
-     Std_ReturnType return_value0 = RTE_Update(&R1_Spd, &data, sizeof(MyUint16OfVendorID));
+     Std_ReturnType return_value0 = IocWrite_Q1_SND1(data);
+     if(return_value0 == IOC_E_NOK) {
+          return RTE_E_NOK;
+     }
      return RTE_E_OK;
 }
 Std_ReturnType Rte_Invalidate_PpIfVehSpd_Spd(){
 
      // Write the invalid value that config in SwDataDefProps.InvalidValue
      Impl_uint16 data = 255;
-     Std_ReturnType return_value0 = RTE_Update(&R1_Spd, &data, sizeof(MyUint16OfVendorID));
+     Std_ReturnType return_value0 = IocWrite_Q1_SND1(data);
+     if(return_value0 == IOC_E_NOK) {
+          return RTE_E_NOK;
+     }
      return RTE_E_OK;
 }
 void RTE_RUNNABLE_ReTxSpd(){
